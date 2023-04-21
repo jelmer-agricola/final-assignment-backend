b8e4c92e9c50eade407d34d51b1da6daea83050e
jelmer-agricola <jelmer.agricola@gmail.com>
2023-04-21 13:36:05 +0200

    opzet op main

---
+++ src/main/java/nl/autogarage/finalassignmentbackendmain/FinalAssignmentBackendMainApplication.java
@@ -0,0 +1,13 @@
+package nl.autogarage.finalassignmentbackendmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssignmentBackendMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAssignmentBackendMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/autogarage/finalassignmentbackendmain/FinalAssignmentBackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.autogarage.finalassignmentbackendmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAssignmentBackendMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
