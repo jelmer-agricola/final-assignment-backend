2cc5910f0d78afc475d4adb78bfab0c1e61a6445
jelmer-agricola <jelmer.agricola@gmail.com>
2023-07-24 10:00:50 +0200

    add

--- src/main/java/nl/autogarage/finalassignmentbackendmain/config/SpringSecurityConfig.java
+++ src/main/java/nl/autogarage/finalassignmentbackendmain/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package nl.autogarage.finalassignmentbackendmain.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Hier staan al je endpoints in
 
 public class SpringSecurityConfig {
 
